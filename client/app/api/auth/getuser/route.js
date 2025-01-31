20d2b8e407bb0b5d9596034075a227129fe98392
yesutkarsh <contactutkarshverma@gmail.com>
2025-01-31 05:52:57 +0530

    DAY3 Added Mongo DB TO authorize user and admin page

--- client/app/api/auth/getuser/route.js
+++ client/app/api/auth/getuser/route.js
@@ -1,11 +1,15 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextResponse } from "next/server";
+
 export async function GET() {
     try {
         const { getUser } = getKindeServerSession();
         const user = await getUser();
-        return NextResponse.json(user)
-    }catch (error) {
-        return error;
+        return NextResponse.json(user);
+    } catch (error) {
+        return NextResponse.json(
+            { message: "Failed to fetch user", error: error.message },
+            { status: 500 }
+        );
     }
 }
